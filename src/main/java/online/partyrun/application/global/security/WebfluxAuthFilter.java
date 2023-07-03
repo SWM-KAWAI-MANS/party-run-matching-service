874f61ecc755e256b50f141f49e5d30365c2f5ed
Hyeonjun Park <phjppo0918@gmail.com>
2023-07-03 09:45:36 +0900

    feat : Configuration Security #PAR-105

---
+++ src/main/java/online/partyrun/application/global/security/WebfluxAuthFilter.java
@@ -0,0 +1,57 @@
+package online.partyrun.application.global.security;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import online.partyrun.jwtmanager.JwtExtractor;
+import online.partyrun.jwtmanager.dto.JwtPayload;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * Webflux 환경에서 인가 필터를 구성합니다. JWT 토큰 기반으로 인가를 진행합니다.
+ *
+ * @author parkhyeonjun
+ * @see JwtExtractor
+ * @see online.partyrun.jwtmanager.manager.JwtManager
+ * @since 2023.06.29
+ */
+@Component
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class WebfluxAuthFilter implements WebFilter {
+    JwtExtractor jwtExtractor;
+    /**
+     * filter가 수행할 떄 request header에서 token을 추출하여 해당 정보를 AuthUser로 생성합니다.
+     *
+     * @author parkhyeonjun
+     * @since 2023.06.29
+     */
+    @Override
+    public Mono<Void> filter(final ServerWebExchange exchange, final WebFilterChain chain) {
+        final JwtPayload payload = getJwtPayload(exchange);
+        return ReactiveSecurityContextHolder.getContext()
+                .doOnNext(c -> c.setAuthentication(new AuthUser(payload)))
+                .then(chain.filter(exchange));
+    }
+
+    /**
+     * header에서 token을 추출합니다.
+     *
+     * @author parkhyeonjun
+     * @since 2023.06.29
+     */
+    private JwtPayload getJwtPayload(final ServerWebExchange exchange) {
+        final String token = exchange.getRequest().getHeaders()
+                .get("Authorization")
+                .stream()
+                .findFirst()
+                .orElseThrow(AuthorizationException::new);
+        final JwtPayload payload = jwtExtractor.extract(token);
+        return payload;
+    }
+}
---
+++ src/main/java/online/partyrun/application/global/security/WebfluxSecurityConfig.java
@@ -0,0 +1,75 @@
+package online.partyrun.application.global.security;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * Webflux 환경에서 Security를 구성합니다.
+ *
+ * @author parkhyeonjun
+ * @since 2023.06.29
+ */
+@Configuration
+@EnableWebFluxSecurity
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class WebfluxSecurityConfig {
+    WebfluxAuthFilter webfluxAuthFilter;
+
+    /**
+     * Webflux 환경에서 {@link SecurityWebFilterChain}을 구성합니다.
+     *
+     * @author parkhyeonjun
+     * @since 2023.06.29
+     */
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+        return http.authorizeExchange(getAuthorizeExchangeSpecCustomizer())
+                .addFilterAfter(webfluxAuthFilter, SecurityWebFiltersOrder.AUTHORIZATION)
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
+                .build();
+
+    }
+
+    /**
+     * CORS 정책을 설정합니다.
+     *
+     * @author parkhyeonjun
+     * @since 2023.06.29
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("https://*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    /**
+     * 각 경로에 관하여 필요 role을 정의합니다.
+     *
+     * @author parkhyeonjun
+     * @since 2023.06.29
+     */
+    private Customizer<ServerHttpSecurity.AuthorizeExchangeSpec> getAuthorizeExchangeSpecCustomizer() {
+        return r -> r.pathMatchers("/**").permitAll();
+    }
+
+}
