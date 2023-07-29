281646410ce3deb9e5dde42e74f748da84dfbc32
Hyeonjun Park <55674648+phjppo0918@users.noreply.github.com>
2023-07-29 13:21:24 +0900

    refactor : matchingService 로직 메서드 추출 리펙터링 (#21)
    기존에 matching service 로직 중 메서드 부분이 너무 길이가 길어 가독성이 떨어지는 문제가 있었습니다. 메서드 추출을 통해 한 메서드 길이를 단축하여 한 메서드가 한 기능을 위주로 가지게 하여 가독성 및 재사용성을 높이기위해 다음과 같이 리펙터링 진행했습니다.

--- src/main/java/online/partyrun/partyrunmatchingservice/domain/matching/service/MatchingService.java
+++ src/main/java/online/partyrun/partyrunmatchingservice/domain/matching/service/MatchingService.java
@@ -11,12 +11,14 @@ import online.partyrun.partyrunmatchingservice.domain.matching.entity.MatchingMe
 import online.partyrun.partyrunmatchingservice.domain.matching.entity.MatchingMemberStatus;
 import online.partyrun.partyrunmatchingservice.domain.matching.repository.MatchingRepository;
 
+import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -35,63 +37,68 @@ public class MatchingService {
         memberIds.forEach(matchingSinkHandler::disconnectIfExist);
         matchingRepository
                 .findAllByMembersIdInAndMembersStatus(memberIds, MatchingMemberStatus.NO_RESPONSE)
-                .flatMap(
-                        matching -> {
-                            matching.cancel();
-                            return matchingRepository.save(matching);
-                        })
+                .flatMap(cancelMatching())
                 .subscribe();
+    }
+
+    private Function<Matching, Publisher<Matching>> cancelMatching() {
+        return matching -> {
+            matching.cancel();
+            return matchingRepository.save(matching);
+        };
     }
 
     private Mono<Matching> saveMatchAndSendEvents(List<MatchingMember> members, int distance) {
         return matchingRepository
                 .save(new Matching(members, distance))
-                .doOnSuccess(
-                        match ->
-                                members.forEach(
-                                        member -> {
-                                            matchingSinkHandler.create(member.getId());
-                                            matchingSinkHandler.sendEvent(
-                                                    member.getId(), new MatchEvent(match));
-                                        }));
+                .doOnSuccess(match -> members.forEach(member -> createSink(match, member)));
+    }
+
+    private void createSink(final Matching match, final MatchingMember member) {
+        matchingSinkHandler.create(member.getId());
+        matchingSinkHandler.sendEvent(member.getId(), new MatchEvent(match));
     }
 
     public Mono<Matching> setMemberStatus(
             final Mono<String> member, final MatchingRequest request) {
-        return member.flatMap(
-                        mid ->
-                                matchingRepository
-                                        .findByMembersIdAndMembersStatus(
-                                                mid, MatchingMemberStatus.NO_RESPONSE)
-                                        .flatMap(
-                                                match ->
-                                                        matchingRepository
-                                                                .updateMatchingMemberStatus(
-                                                                        match.getId(),
-                                                                        mid,
-                                                                        MatchingMemberStatus
-                                                                                .getByIsJoin(
-                                                                                        request
-                                                                                                .isJoin()))
-                                                                .then(
-                                                                        Mono.defer(
-                                                                                () ->
-                                                                                        matchingRepository
-                                                                                                .findById(
-                                                                                                        match
-                                                                                                                .getId())))))
-                .flatMap(
-                        match -> {
-                            match.updateStatus();
-                            if (!match.isWait()) {
-                                return matchingRepository.save(match);
-                            }
-                            return Mono.just(match);
-                        })
-                .doOnSuccess(this::sendEvent);
+        return member.flatMap(memberId -> updateMatchMemberStatus(request, memberId))
+                .flatMap(this::updateMatchStatus)
+                .doOnSuccess(this::multiCastEvent);
     }
 
-    private void sendEvent(final Matching matching) {
+    private Mono<Matching> updateMatchMemberStatus(
+            final MatchingRequest request, final String memberId) {
+        return findMatchingByNoResponseMember(memberId)
+                .flatMap(updateMatching(request.isJoin(), memberId));
+    }
+
+    private Mono<Matching> findMatchingByNoResponseMember(final String memberId) {
+        return matchingRepository.findByMembersIdAndMembersStatus(
+                memberId, MatchingMemberStatus.NO_RESPONSE);
+    }
+
+    private Function<Matching, Mono<Matching>> updateMatching(
+            final boolean isJoin, final String memberId) {
+        return matching -> findByMemberIdAndUpdate(isJoin, memberId, matching.getId());
+    }
+
+    private Mono<Matching> findByMemberIdAndUpdate(
+            final boolean isJoin, final String memberId, final String matchingId) {
+        return matchingRepository
+                .updateMatchingMemberStatus(
+                        matchingId, memberId, MatchingMemberStatus.getByIsJoin(isJoin))
+                .then(Mono.defer(() -> matchingRepository.findById(matchingId)));
+    }
+
+    private Mono<Matching> updateMatchStatus(final Matching match) {
+        match.updateStatus();
+        if (!match.isWait()) {
+            return matchingRepository.save(match);
+        }
+        return Mono.just(match);
+    }
+
+    private void multiCastEvent(final Matching matching) {
         matching.getMembers()
                 .forEach(
                         member ->
