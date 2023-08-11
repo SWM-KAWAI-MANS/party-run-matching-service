280d88e846bb5ad91831cd0c0c7f7569769bfeda
Hyeonjun Park <phjppo0918@gmail.com>
2023-08-11 10:48:27 +0900

    test : cancel 관련 테스트 구현

--- src/test/java/online/partyrun/partyrunmatchingservice/domain/waiting/queue/InMemoryWaitingQueueTest.java
+++ src/test/java/online/partyrun/partyrunmatchingservice/domain/waiting/queue/InMemoryWaitingQueueTest.java
@@ -1,20 +1,20 @@
 package online.partyrun.partyrunmatchingservice.domain.waiting.queue;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import online.partyrun.partyrunmatchingservice.domain.waiting.exception.DuplicateMemberException;
 import online.partyrun.partyrunmatchingservice.domain.waiting.exception.NotSatisfyCountException;
 import online.partyrun.partyrunmatchingservice.domain.waiting.root.WaitingMember;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("InMemoryWaitingQueue")
 class InMemoryWaitingQueueTest {
     WaitingQueue waitingQueue = new InMemoryWaitingQueue();
     WaitingMember 현준 = new WaitingMember("현준", 1000);
     WaitingMember 성우 = new WaitingMember("셩우", 1000);
+    WaitingMember 준혁 = new WaitingMember("준혁", 1000);
 
     @Test
     @DisplayName("큐에 추가를 수행한 후 개수가 만족한 지 확인한다")
@@ -58,6 +58,18 @@ class InMemoryWaitingQueueTest {
         waitingQueue.clear();
 
         assertThat(waitingQueue.hasMember(현준.memberId())).isFalse();
+        assertThat(waitingQueue.hasMember(성우.memberId())).isFalse();
+    }
+
+    @Test
+    @DisplayName("원하는 user를 삭제한다")
+    void deleteOne() {
+        waitingQueue.add(현준);
+        waitingQueue.add(성우);
+        waitingQueue.add(준혁);
+
+        waitingQueue.delete(성우.memberId());
+
         assertThat(waitingQueue.hasMember(성우.memberId())).isFalse();
     }
 }
--- src/test/java/online/partyrun/partyrunmatchingservice/domain/waiting/service/WaitingEventServiceTest.java
+++ src/test/java/online/partyrun/partyrunmatchingservice/domain/waiting/service/WaitingEventServiceTest.java
@@ -1,25 +1,30 @@
 package online.partyrun.partyrunmatchingservice.domain.waiting.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import online.partyrun.partyrunmatchingservice.config.redis.RedisTestConfig;
+import online.partyrun.partyrunmatchingservice.domain.waiting.dto.CreateWaitingRequest;
 import online.partyrun.partyrunmatchingservice.domain.waiting.dto.WaitingEventResponse;
 import online.partyrun.partyrunmatchingservice.domain.waiting.dto.WaitingStatus;
-
+import online.partyrun.partyrunmatchingservice.domain.waiting.queue.WaitingQueue;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.context.annotation.Import;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DisplayName("WaitingEventService")
 @SpringBootTest
+@Import(RedisTestConfig.class)
 class WaitingEventServiceTest {
     @Autowired WaitingEventService waitingEventService;
     @Autowired WaitingService waitingService;
     @Autowired WaitingSinkHandler waitingSinkHandler;
+    @Autowired
+    WaitingQueue waitingQueue;
 
     Mono<String> user1 = Mono.just("현준");
 
@@ -62,5 +67,15 @@ class WaitingEventServiceTest {
         waitingEventService.shutdown();
 
         assertThat(waitingSinkHandler.getConnectors()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("매칭 중 취소하면 sink를 삭제하고, 대기queue에도 삭제한다")
+    void runCancel() {
+        waitingService.create(user1, new CreateWaitingRequest(1000)).block();
+
+        waitingEventService.getEventStream(user1).subscribe().dispose();
+        assertThat(waitingQueue.hasMember(user1.block())).isFalse();
+        assertThat(waitingSinkHandler.getConnectors()).isNotIn(user1.block());
     }
 }
