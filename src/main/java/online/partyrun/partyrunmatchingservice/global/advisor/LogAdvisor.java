075c9fbf9cf67e588c0bddc6e7922afc1b52480f
Hyeonjun Park <phjppo0918@gmail.com>
2023-08-17 14:28:00 +0900

    feat : 전역 Debug log 설정

---
+++ src/main/java/online/partyrun/partyrunmatchingservice/global/advisor/LogAdvisor.java
@@ -0,0 +1,28 @@
+package online.partyrun.partyrunmatchingservice.global.advisor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class LogAdvisor {
+
+    //partyrunservice 속한 모든 패키지의 모든 클래스의 모든 메서드
+    @Pointcut("execution(* online.partyrun.partyrunmatchingservice..*.*(..))")
+    public void allComponents(){}
+
+
+    @Before("allComponents()")
+    public void generateTraceLong(JoinPoint joinPoint)  {
+        log.debug(
+                "METHOD : {}, ARGS : {}",
+                joinPoint.getSignature().toShortString(),
+                joinPoint.getArgs());
+
+    }
+}
