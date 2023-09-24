bd2872b38c98925e414edad7e7926f8ff9bb1127
Hyeonjun Park <phjppo0918@gmail.com>
2023-09-24 15:33:13 +0900

    refactor : timeout을 핸들러 별로 별도 설정이 가능하게 변경

--- src/main/java/online/partyrun/partyrunmatchingservice/domain/party/service/PartySinkHandler.java
+++ src/main/java/online/partyrun/partyrunmatchingservice/domain/party/service/PartySinkHandler.java
@@ -4,6 +4,14 @@ import online.partyrun.partyrunmatchingservice.domain.party.dto.PartyEvent;
 import online.partyrun.partyrunmatchingservice.global.sse.SinkHandlerTemplate;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+
 @Component
 public class PartySinkHandler extends SinkHandlerTemplate<String, PartyEvent> {
+    private static final int PARTY_TIME_OUT_MINUTES = 10;
+
+    @Override
+    protected Duration timeout() {
+        return Duration.ofMinutes(PARTY_TIME_OUT_MINUTES);
+    }
 }
